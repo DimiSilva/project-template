bf30e2b5c5d29344a09d589e30d2ae9bff0ccace
Dimi Silva <jnn.sla@gmail.com>
2022-06-17 10:33:40 -0300

    helmet implementation in api

--- api/config/express.ts
+++ api/config/express.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from 'helmet'
 
 import setupRoutes from "../routes";
 
@@ -9,16 +10,17 @@ import { middlewares } from "backend-common";
 const setupExpress = (): Express => {
   const app = express();
 
+  app.use(helmet())
+  app.use(cors());
+  app.use(morgan("dev"));
+  app.use(express.json());
+
   app.get("/status", (_, res) => {
     res.status(200).end();
   });
   app.head("/status", (_, res) => {
     res.status(200).end();
   });
-
-  app.use(cors());
-  app.use(morgan("dev"));
-  app.use(express.json());
 
   setupRoutes(app);
 
